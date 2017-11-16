64978cf75bbf8b03fbb6e28f784c76111b2b8973
Sebastian <sebachem@gmx.de>
2017-11-15 22:50:59 -0600

    Create Empty Activities for all Screens

---
+++ app/src/main/java/nrdzs/cs465/illinois/edu/spot/DetailedPhotoActivity.java
@@ -0,0 +1,13 @@
+package nrdzs.cs465.illinois.edu.spot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailedPhotoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_photo);
+    }
+}
---
+++ app/src/main/java/nrdzs/cs465/illinois/edu/spot/FloorAndPhotoActivity.java
@@ -0,0 +1,13 @@
+package nrdzs.cs465.illinois.edu.spot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FloorAndPhotoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floor_and_photo);
+    }
+}

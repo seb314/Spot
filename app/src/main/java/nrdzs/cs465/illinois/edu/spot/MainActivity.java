17a93f636727ff1609027b9547cd27d2061c08da
Dashiell Koester <dashkoester@gmail.com>
2017-11-24 23:14:03 -0600

    expandable listview implemented

--- app/src/main/java/nrdzs/cs465/illinois/edu/spot/MainActivity.java
+++ app/src/main/java/nrdzs/cs465/illinois/edu/spot/MainActivity.java
@@ -9,6 +9,18 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -17,6 +29,13 @@ import java.io.IOException;
 import nrdzs.cs465.illinois.edu.spot.R;
 
 public class MainActivity extends CustomActivity {
+
+    /*Expandable List View*/
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +54,61 @@ public class MainActivity extends CustomActivity {
 
         // bind an imageview so we could see the taken picture
         mImageView = (ImageView) findViewById(R.id.image_taken);
+
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding Library Titles
+        listDataHeader.add("Grainger");
+        listDataHeader.add("UGL");
+        listDataHeader.add("Main Library");
+        listDataHeader.add("ACES");
+
+        // Adding child data to each library
+        List<String> Grainger = new ArrayList<String>();
+        Grainger.add("Basement");
+        Grainger.add("First Floor");
+        Grainger.add("Second Floor");
+        Grainger.add("Third Floor");
+        Grainger.add("Fourth Floor");
+
+        List<String> UGL = new ArrayList<String>();
+        UGL.add("Lower Level");
+        UGL.add("Upper Level");
+
+        List<String> MainLibrary = new ArrayList<String>();
+        MainLibrary.add("First Floor");
+        MainLibrary.add("Second Floor");
+        MainLibrary.add("Third Floor");
+        MainLibrary.add("Fourth Floor");
+
+        List<String> ACES = new ArrayList<String>();
+        ACES.add("Basement");
+        ACES.add("First Floor");
+        ACES.add("Second Floor");
+        ACES.add("Third Floor");
+        ACES.add("Fourth Floor");
+        ACES.add("Fifth Floor");
+
+
+        listDataChild.put(listDataHeader.get(0), Grainger); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), UGL);
+        listDataChild.put(listDataHeader.get(2), MainLibrary);
+        listDataChild.put(listDataHeader.get(3), ACES);
     }
 
 

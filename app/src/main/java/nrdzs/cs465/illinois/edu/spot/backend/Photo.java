be43bf086c425e5bd801ef977a954b8c8771d61d
Sebastian <sebachem@gmx.de>
2017-11-28 20:13:34 -0600

    first draft for more organized data structure

---
+++ app/src/main/java/nrdzs/cs465/illinois/edu/spot/backend/Photo.java
@@ -0,0 +1,12 @@
+package nrdzs.cs465.illinois.edu.spot.backend;
+
+import android.net.Uri;
+
+/**
+ * Created by sebastian on 28.11.17.
+ */
+
+public class Photo {
+    private Uri uri;
+    // maybe don't store a reference to the library/floor here to avoid cyclic dependencies?
+}

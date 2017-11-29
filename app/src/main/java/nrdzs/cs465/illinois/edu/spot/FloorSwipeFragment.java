8af185d1191d71d6cff4e9f52a7c523883293e7a
Sebastian <sebachem@gmx.de>
2017-11-28 22:35:42 -0600

    some debugging info

--- app/src/main/java/nrdzs/cs465/illinois/edu/spot/FloorSwipeFragment.java
+++ app/src/main/java/nrdzs/cs465/illinois/edu/spot/FloorSwipeFragment.java
@@ -3,6 +3,7 @@ package nrdzs.cs465.illinois.edu.spot;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,6 +43,12 @@ public class FloorSwipeFragment  extends Fragment {
         });
 
         return rootView;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("Fragment", "Fragment destroyed");
     }
 
 
